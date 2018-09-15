2fa0b9bfc1a1c3c180c654cba9da230d06483c61
EvilaMany <lineman98@yandex.ru>
2018-09-15 19:11:18 +0300

    more than half of solution ninth task

--- src/index.js
+++ src/index.js
@@ -16,53 +16,82 @@ function solveSudoku(matrix) {
         }
     }
 
-    for (let II = 0; II < 10; II++) {
-        //преебираем все строки
-        for (let y in matrix) {
-            for (let x in matrix[y]) {
-                if (typeof (matrix[y][x]) != "object") continue;
- 
- 
-                for (let number of matrix[y][x]) {
-                    var blockY = Math.floor(y / 3) * 3 //границы блока
-                    var blockX = Math.floor(x / 3) * 3
+    matrix = findObviousNumbers(matrix)
 
- 					var setting = false
- 					//единственное возможное число в ячейке
- 					if(onlyPossibleNumber(number,matrix,y,x) == true)
- 					{
- 						matrix[y][x] = number;
-                        setting = true 
- 					}
-                    //строка
-                    if (possibleCountLength(number, matrix, y, 0, y, 8) == 1) {
-                         matrix[y][x] = number;
-                         setting = true
-                    }
-                    //столбец
-                    if (possibleCountLength(number, matrix, 0, x, 8, x) == 1) {
-                        matrix[y][x] = number;
-                        setting = true 
-                    }
-                    //блок
- 
-                    if (possibleCountLength(number, matrix, blockY, blockX, blockY + 2, blockX + 2) == 1) {
-                        matrix[y][x] = number;
-                        setting = true;
-                    } 
-                    if(setting == true)
-                    {
-                    	matrix = removePossibleNumber(number, matrix, 0, x, 8, x);
-                        matrix = removePossibleNumber(number, matrix, y, 0, y, 8);
-                        matrix = removePossibleNumber(number, matrix, blockY, blockX, blockY + 2, blockX + 2);
-                    	break;
-                    }
-                }
-            }
-        }
+
+    if(isSolvedMatrix(matrix) != true)
+    for(var y in matrix)
+    {
+    	for(var x in matrix[y])
+    	{
+    		if(typeof(matrix[y][x]) == "object")
+    		{
+    			for(var possibleNumber of matrix[y][x])
+    			{
+    				var matrixCopy = JSON.parse(JSON.stringify(matrix));
+
+    				matrixCopy = putNumber(possibleNumber,matrixCopy,y,x)
+
+    				matrixCopy = findObviousNumbers(matrixCopy)
+    				if(isSolvedMatrix(matrixCopy))
+    				{
+    					return matrixCopy;
+    				}
+
+    			}
+    		}
+    	}
     }
+
     return printMatrix(matrix)
  
+
+    function isSolvedMatrix(matrix)
+    {
+    	for(var y in matrix)
+    		for(var x in matrix[y])
+    			if(typeof(matrix[y][x]) != 'number' || 
+    				matrix[y][x] == 0 || 
+    				(new Set(matrix[y])).size !== matrix[y].length)
+    				return false;
+    	return true;
+    }
+
+    function findObviousNumbers(matrix)
+    {
+    	for (let II = 0; II < 100; II++) {
+	    	var edited = false
+	        //преебираем все строки
+	        for (let y in matrix) {
+	            for (let x in matrix[y]) {
+	                if (typeof (matrix[y][x]) != "object") continue;
+	 
+	 
+	                for (let number of matrix[y][x]) {
+	                    var blockY = Math.floor(y / 3) * 3 //границы блока
+	                    var blockX = Math.floor(x / 3) * 3
+
+	 					if(	
+	 						isOnlyPossibleNumber(number,matrix,y,x) == true || //единственное возможное число в ячейке	
+	 						possibleCountLength(number, matrix, y, 0, y, 8) == 1 ||  //строка
+	 						possibleCountLength(number, matrix, 0, x, 8, x) == 1 ||	 //столбец
+	 						possibleCountLength(number, matrix, blockY, blockX, blockY + 2, blockX + 2) == 1 //блок
+	 					  )
+	 					{
+	 						matrix = putNumber(number,matrix,y,x);
+	 						edited = true
+	                    	break;
+	 					}
+	                }
+	            }
+	        }
+	        if(edited == false)
+	        {
+        		return(matrix);
+	        }
+    	}
+    }
+
     function printMatrix(matrix)
     {
     	for(var y in matrix)
@@ -72,7 +101,7 @@ function solveSudoku(matrix) {
 					//matrix[y][x] = matrix[y][x].join('+')
 		return matrix;
     }
-    function onlyPossibleNumber(number,matrix,y,x)
+    function isOnlyPossibleNumber(number,matrix,y,x)
     {	 
     	for(let n of matrix[y][x])
     	{
@@ -81,6 +110,26 @@ function solveSudoku(matrix) {
     	}
     	return true;
     }
+
+    /*
+	Вписывает число в ячейку, при этом удаляя это число из возможных в столбе, строке и блоке
+    */
+    function putNumber(number,matrix,y,x)
+    {
+    	var blockY = Math.floor(y / 3) * 3 //границы блока
+        var blockX = Math.floor(x / 3) * 3
+
+    	matrix[y][x] = number;
+        matrix = removePossibleNumber(number, matrix, 0, x, 8, x);
+        matrix = removePossibleNumber(number, matrix, y, 0, y, 8);
+        matrix = removePossibleNumber(number, matrix, blockY, blockX, blockY + 2, blockX + 2);
+
+        return matrix;
+	}
+	
+	/*
+	Определяет возможные номера в переданной ячейке
+	*/
     function detectPossibleNumbers(y,x,matrix)
     {
         //нашли все цифры в строке
@@ -112,6 +161,9 @@ function solveSudoku(matrix) {
         return possibleNumbers;
     }
  
+ 	/*
+	Определяет в скольки ячейках в переданном диапозоне ячеек переданное число содержится в "возможных"
+ 	*/
     function possibleCountLength(number,matrix, y1, x1, y2, x2) {
         let count = 0;
         for (let y = y1; y <= y2; y++) {
@@ -125,6 +177,9 @@ function solveSudoku(matrix) {
         return count;
     }
  
+    /*
+	Удаляет число из "возможных" в переданном диапозоне ячеек
+    */
     function removePossibleNumber(number, matrix, y1, x1, y2, x2) {
         for (let y = y1; y <= y2; y++) {
             for (let x = x1; x <= x2; x++) {
@@ -139,16 +194,15 @@ function solveSudoku(matrix) {
     }
 }
  
- /*
-console.log(solveSudoku([
-    [0, 5, 0, 4, 0, 0, 0, 1, 3],
-    [0, 2, 6, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 9, 0],
-    [0, 0, 0, 0, 8, 5, 6, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 6, 0, 0, 0, 0],
-    [3, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 7, 3, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 5, 0, 0]
-  ]))
-*/
+ 
+/*console.log(solveSudoku([
+	[0, 5, 0, 0, 7, 0, 0, 0, 1],
+    [8, 7, 6, 0, 2, 1, 9, 0, 3],
+    [0, 0, 0, 0, 3, 5, 0, 0, 0],
+    [0, 0, 0, 0, 4, 3, 6, 1, 0],
+    [0, 4, 0, 0, 0, 9, 0, 0, 2],
+    [0, 1, 2, 0, 5, 0, 0, 0, 4],
+    [0, 8, 9, 0, 6, 4, 0, 0, 0],
+    [0, 0, 0, 0, 0, 7, 0, 0, 0],
+    [1, 6, 7, 0, 0, 2, 5, 4, 0]
+  ]))*/
